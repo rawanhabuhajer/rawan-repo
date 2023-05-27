dff5e2cf0f84179cd32ab0742b7d55a758e96c5e
rawan6922&9 <rawanh.abuhajer@gmail.com>
2023-05-27 20:12:34 +0300

    add js

---
+++ js/script.js
@@ -0,0 +1,28 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".box");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+
+
+  document.getElementById("dropdown-img").addEventListener("click", function() {
+    
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
